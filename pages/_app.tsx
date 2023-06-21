6bbc16fd620b09745565d5419695965919066d1e
Duy Anh <ndanhdev@gmail.com>
2023-06-21 16:50:15 +0700

    update font-awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 // import Toastify CSS
 import 'react-toastify/dist/ReactToastify.css';
 import '@/styles/globals.scss'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
