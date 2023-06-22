7ce62d7770cc8819565c54168c85b7cb44833876
Duy Anh <ndanhdev@gmail.com>
2023-06-22 10:06:34 +0700

    header

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='container-fluid'>
+    <div className='container-fluid p-0'>
       <Header />
       <h1>Content</h1>
       <Footer/>
